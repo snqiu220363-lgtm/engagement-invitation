e4f3a3f79777f284f35f3afbe2a81711820659e3
snqiu220363-lgtm <snqiu220363@uniq.edu.iq>
2026-08-02 03:14:26 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,5 @@
 function openEnvelope() {
-    const envelope = document.querySelector('.envelope');
-    
-    // یەکەم: زەرفەکە دەکرێتەوە
-    envelope.classList.add('open');
-
-    // دووەم: پاش یەک چرکە، دەچێتە ناوەوەی نامەکە
-    setTimeout(() => {
-        document.getElementById('envelope-screen').classList.remove('active');
-        document.getElementById('invitation-screen').classList.add('active');
-    }, 1000);
+    // داپۆشینی زەرفەکە و پیشاندانی ناوەوەی نامەکە
+    document.getElementById('envelope-screen').classList.remove('active');
+    document.getElementById('invitation-screen').classList.add('active');
 }
